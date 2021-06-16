632b02846db9cab26a1684d8a4af0cef8f755179
WangXiaoYong <plortinus@gmail.com>
2021-06-16 09:47:04 +0800

    feat: c-single-input实现

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -10,9 +10,10 @@
   <div class="container" >
     <c-button>默认按钮</c-button>
     <c-button type="primary">主要按钮</c-button>
-    <c-button ghost>ghost</c-button>
-    <c-button loading>loading</c-button>
     <hr />
     <c-query-view></c-query-view>
+    <c-single-input label="手机号" placeholder="占位符1" attr="phone" />
+    <c-single-input label="验证码" placeholder="占位符2" ismust="true" attr="code" />
+    <c-single-input label="测试label/wrapper样式" labelcol="8" wrappercol="10" placeholder="占位符1" attr="phone" />
   </div>
 </main>
---
+++ lib/CSingleInput/index.wc.svelte
@@ -0,0 +1,42 @@
+<svelte:options tag="c-single-input"/>
+
+<script>
+  export let id
+  export let placeholder
+  export let label
+  export let attr
+  export let ismust
+  export let value
+  export let labelcol = 8
+  export let wrappercol = 16
+  $: required =  !!ismust
+
+  $: labelclass = `ant-col ant-col-${labelcol} ant-form-item-label`
+  $: wrapperclass = `ant-col ant-col-${wrappercol} ant-form-item-control`
+</script>
+
+<div class="ant-row ant-form-item" style="row-gap: 0px;">
+  <div class={labelclass}>
+    <label for="basic_username" class:ant-form-item-required="{required}">
+      {label}
+    </label>
+  </div>
+  <div class={wrapperclass}>
+    <div class="ant-form-item-control-input">
+      <div class="ant-form-item-control-input-content">
+        <input
+          type="text"
+          id={id}
+          placeholder={placeholder}
+          class="ant-input"
+          name={attr}
+          value={value || ''}
+          >
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  @import url('https://unpkg.com/antd@4.16.2/dist/antd.min.css');
+</style>
